cc9e323cdc8502db3d6d93e48305e74650e1c9ca
Dig Kleppe <D.Kleppe@kpnmail.nl>
2023-06-22 11:08:23 +0200

    added wavescope

--- src/audio.c
+++ src/audio.c
@@ -3,6 +3,13 @@
  *
  *  Created on: Jun 13, 2023
  *      Author: dig
+ *
+ *
+ *
+ gst-launch-1.0 udpsrc port=5002 caps='application/x-rtp, media=(string)audio, clock-rate=(int)44100, encoding-name=(string)L16, encoding-params=(string)1, channels=(int)1, payload=(int)96' ! rtpL16depay ! audioconvert ! autoaudiosink
+
+ gst-launch-1.0 udpsrc port=5002 caps='application/x-rtp, media=(string)audio, clock-rate=(int)44100, encoding-name=(string)L16, encoding-params=(string)1, channels=(int)1, payload=(int)96' ! rtpL16depay ! tee name= t t. ! queue  ! audioconvert ! autoaudiosink t.! queue ! audioconvert ! wavescope ! ximagesink
+ *
  */
 
 #include <stdbool.h>
@@ -24,8 +31,8 @@ GstElement *audioPipeline[2]; // only works global??
 
 GstStateChangeReturn startAudio(int UDPport, int idx) {
 
-	GstElement *audioSource, *rtpL16depay, *audioconvert,*volume, * audiopanorama, * audioresample ,*audiosink;
-	GstElement *mpegaudioparser,* mpg123audiodec;
+	GstElement *audioSource, *rtpL16depay, *audioconvert, *audioconvert2, *volume, *audiopanorama, *audioresample, *audiosink, *tee;
+	GstElement *queue, *queue2, *wavescope, *ximagesink;
 
 	bool error = false;
 	GstCaps *caps = NULL;
@@ -46,36 +53,51 @@ GstStateChangeReturn startAudio(int UDPport, int idx) {
 	g_object_set(volume, "volume", HANDSETVOLUME, NULL);
 
 	audiosink = gst_element_factory_make("autoaudiosink", "autoaudiosink");
+	gst_element_factory_make("audioconvert", "audioconvert");
 
+	tee = gst_element_factory_make("tee", "tee");
+	g_object_set(tee, "name", "t", NULL);
+
+	audioconvert2 = gst_element_factory_make("audioconvert", "audioconvert2");
+	queue = gst_element_factory_make("queue", "queue");
+	queue2 = gst_element_factory_make("queue", "queue2");
+	wavescope = gst_element_factory_make("wavescope", "wavescope");
+	ximagesink = gst_element_factory_make("ximagesink", "ximagesink");
 
 //	audiosink = gst_element_factory_make("alsasink", "alsasink");
 //	g_object_set(audiosink, "device", devicename, NULL);
 
-	if (!audioSource || !rtpL16depay || !audioconvert || !audioresample
-			|| !audiopanorama || !audiosink || !volume) {
+	if (!audioSource || !rtpL16depay || !audioconvert || !audioresample || !audiopanorama || !audiosink || !volume || !tee || !queue || !queue2
+			|| !audioconvert2 || !wavescope || !ximagesink) {
 		g_printerr("Not all audio elements could be created.\n");
 		error = true;
 	}
 
 	audioPipeline[idx] = gst_pipeline_new("receive audiopipeline");
-	gst_bin_add_many(GST_BIN(audioPipeline[idx]), audioSource, rtpL16depay,
-			audioconvert, audioresample, volume, audiopanorama, audiosink,
+	gst_bin_add_many(GST_BIN(audioPipeline[idx]), audioSource, rtpL16depay, audioconvert, audioconvert2, audioresample, volume, audiopanorama, audiosink, tee,
+			queue, queue2, wavescope, ximagesink,
 			NULL);
 
-	caps = gst_caps_new_simple("application/x-rtp", "media", G_TYPE_STRING,
-			"audio",
-			//	"clock-rate", G_TYPE_INT,24000, // logitec
-			"clock-rate", G_TYPE_INT,
-			44100, // USB sound
-			"encoding-name", G_TYPE_STRING, "L16", "encoding-params",
-			(G_TYPE_STRING), "1", "channels", G_TYPE_INT, 1, "payload",
+	caps = gst_caps_new_simple("application/x-rtp", "media", G_TYPE_STRING, "audio",
+	//	"clock-rate", G_TYPE_INT,24000, // logitec
+			"clock-rate", G_TYPE_INT, 44100, // USB sound
+			"encoding-name", G_TYPE_STRING, "L16", "encoding-params", (G_TYPE_STRING), "1", "channels", G_TYPE_INT, 1, "payload",
 			G_TYPE_INT, 96, NULL);
 
 	if (link_elements_with_filter(audioSource, rtpL16depay, caps) != TRUE)
 		error = true;
 
-	if (gst_element_link(rtpL16depay, audioconvert) != TRUE)
+	if (gst_element_link(rtpL16depay, tee) != TRUE)
 		error = true;
+
+	if (gst_element_link(tee, queue) != TRUE)
+		error = true;
+
+	if (gst_element_link(queue, audioconvert) != TRUE)
+		error = true;
+
+//	if (gst_element_link(rtpL16depay, audioconvert) != TRUE)
+//		error = true;
 
 	if (gst_element_link(audioconvert, audioresample) != TRUE)
 		error = true;
@@ -89,14 +111,25 @@ GstStateChangeReturn startAudio(int UDPport, int idx) {
 	if (gst_element_link(audiopanorama, audiosink) != TRUE)
 		error = true;
 
+	if (gst_element_link(tee, queue2) != TRUE)
+		error = true;
+
+	if (gst_element_link(queue2, audioconvert2) != TRUE)
+		error = true;
+
+	if (gst_element_link(audioconvert2, wavescope) != TRUE)
+		error = true;
+
+	if (gst_element_link(wavescope, ximagesink) != TRUE)
+		error = true;
+
 	usleep(10 * 1000);
 	if (error)
 		g_printerr("Elements could not be linked.\n");
 	else {
 		ret = gst_element_set_state(audioPipeline[idx], GST_STATE_PLAYING);
 		if (ret == GST_STATE_CHANGE_FAILURE) {
-			g_printerr(
-					" audio Receive Unable to set the audio pipeline to the playing state.\n");
+			g_printerr(" audio Receive Unable to set the audio pipeline to the playing state.\n");
 			error = true;
 		}
 	}
